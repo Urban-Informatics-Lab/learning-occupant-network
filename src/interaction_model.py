1e547766e5a3106f9ed572637379264d66b4d805
asonta <asonta@stanford.edu>
2020-09-15 14:37:22 -0700

    add license text

--- src/interaction_model.py
+++ src/interaction_model.py
@@ -2,6 +2,21 @@
 This file contains functions for the interaction model, which computes overall
 opportunities for interaction based on time-series state data.
 
+Copyright (C) 2020  Andrew Sonta, Rishee K. Jain
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @author: Andrew Sonta
 """
 
--- src/state_classification.py
+++ src/state_classification.py
@@ -3,6 +3,21 @@ This script performs the activity state classification procedure as described in
 As it is currently set up, it performs a two-step classification process, reclassifying
 the higher energy data if the initial classiciation produces two clusters
 
+Copyright (C) 2020  Andrew Sonta, Rishee K. Jain
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 @author: Andrew Sonta
 """
 
